e7c4a8678764de88a84ea04e81774c5fd1b82bbd
Joseph Hall <josephdahall@hotmail.co.nz>
2023-04-07 15:45:14 +1200

    Updated Python solution to better use python classes.

--- Python/PathFinder.py
+++ Python/PathFinder.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Aug 21 17:11:14 2019
 
@@ -7,19 +5,42 @@ Created on Wed Aug 21 17:11:14 2019
 """
 from math import sqrt
 import sys
-# import time
 
 directions = {"north":-1, "south":1, "east":1, "west":-1}
 
 class Map:
-    def __init__(self,map,path):
+    def __init__(self,map: list[str],path: list[str]):
         self.map = map
         self.curr_pos = []
         self.path = path
-        self.drawPath()
-        
-    def drawPath(self):
-        pos = start(self)
+        self.start_pos = self.__getStartPos()
+        self.goal_pos =  self.__getGoalPos()
+        self.__drawPath()
+
+    def __str__(self):
+        s = ""
+        for m in self.map:
+            s += m
+        return f'{s}\nPath:{self.path}\nPath Length:{len(self.path)}\nCurrent Position:{self.curr_pos}'
+
+    def __getStartPos(self) -> int | list[int]:
+        pos = 0
+        for line in self.map:
+            if(line.find('S')!=-1):
+                return [pos,line.find('S')]
+            pos += 1
+        return -1
+
+    def __getGoalPos(self) -> int | list[int]:
+        pos = 0
+        for line in self.map:
+            if(line.find('G')!=-1):
+                return [pos,line.find('G')]
+            pos += 1
+        return -1
+
+    def __drawPath(self) -> None:
+        pos = self.start_pos
         if pos == -1:
             print("ERROR: No start position in given map file!")
             return
@@ -32,38 +53,16 @@ class Map:
             self.map[pos[0]] = line[0:pos[1]] + '.' + line[pos[1]+1:len(line)]
         self.curr_pos = pos
 
-def readMap(map_name):
+def readMap(map_name) -> str:
     m = open(map_name,'r')
     map = [line for line in m]
     return map
-
-def printMap(map):
-    for m in map.map:
-        print(m.strip())
-    print(map.path)
-    print(f"Current position: {map.curr_pos}")
         
-def start(map):
-    pos = 0
-    for line in map.map:
-        if(line.find('S')!=-1):
-            return [pos,line.find('S')]
-        pos += 1
-    return -1
-
-def goal(map):
-    pos = 0
-    for line in map.map:
-        if(line.find('G')!=-1):
-            return [pos,line.find('G')]
-        pos += 1
-    return -1
-
-def cost(map):
+def cost(map) -> int:
     return len(map.path)
 
 def heuristic(map):
-    goal_pos = goal(map)
+    goal_pos = map.goal_pos
     curr_pos = map.curr_pos
     # √((posX-goalX)^2 + (posY-goalY)^2)
     x_value = (curr_pos[0]-goal_pos[0]) * (curr_pos[0]-goal_pos[0])
@@ -73,7 +72,7 @@ def heuristic(map):
 def heuristicAndCost(map):
     return(heuristic(map)+cost(map))
 
-def expand(map,visted_positions):
+def expand(map,visted_positions) -> list[Map]:
     maps = []
     pos = map.curr_pos
     # check north
@@ -98,23 +97,22 @@ def expand(map,visted_positions):
         maps.append(Map(map.map.copy(),new_path))
     return maps
 
-def searchMap(debug,fileName,search_type):
+def searchMap(debug,fileName,search_type) -> None:
     if search_type not in ["a-star", "best-first", "breadth-first", "depth-first"]:
         print(f"Search type {search_type} is not a valid search type.")
         return
 
     search_map = Map(readMap(fileName),[])
     maps = [search_map]
-    visted_positions = [start(search_map)]
-    goal_pos = goal(maps[0])
+    visted_positions = [search_map.start_pos]
+    goal_pos = maps[0].goal_pos
     num_expansions = 0
     
     while True:
         if(debug):
-            printMap(maps[0])
+            print(maps[0])
             print(f"Number of expansions: {str(num_expansions)}")
-            print(f"Path Length: {str(cost(maps[0]))}")
-            # time.sleep(0.2)
+            time.sleep(1)
         
         num_expansions += 1
         for m in expand(maps.pop(0),visted_positions):
@@ -124,9 +122,8 @@ def searchMap(debug,fileName,search_type):
                 goal_line = goal_line[0:goal_pos[1]] + 'G' +  goal_line[goal_pos[1]+1:len(goal_line)]
                 m.map[goal_pos[0]] = goal_line
 
-                printMap(m)
+                print(m)
                 print(f"Number of expansions: {str(num_expansions)}")
-                print(f"Path Length: {str(cost(m))}")
                 return
 
             if(search_type in ["a-star", "best-first", "breadth-first"]):
@@ -140,14 +137,14 @@ def searchMap(debug,fileName,search_type):
         elif(search_type=="best-first"):
             maps = sorted(maps,key=heuristic)            
 
-def main():
+def main() -> None:
     # alogrithms = ["a-star", "best-first", "breadth-first", "depth-first"]
     if len(sys.argv) == 4:
         searchMap(True,str(sys.argv[2]),str(sys.argv[3]))
     elif len(sys.argv) == 3:
         searchMap(False,str(sys.argv[1]),str(sys.argv[2]))
     else:
-        searchMap(False,"./map2.txt","a-star")
+        searchMap(False,"../Maps/map2.txt","a-star")
 
 if __name__ == "__main__":  
     main()
